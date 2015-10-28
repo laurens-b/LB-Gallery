b83f1f89d8b8138b9f5fce283545712a5b760316
laurens-b <github@laurensbakker.nl>
2015-10-28 20:46:14 +0100

    $config ['filter_fields']
    Add 'white-list' of allowed filter_fields to constructor and cleaned up
    GetListQuery().

--- admin/models/items.php
+++ admin/models/items.php
@@ -13,6 +13,34 @@ defined('_JEXEC') or die('Restricted access');
 class LBGalleryModelItems extends JModelList
 {
 	/**
+	 * Constructor.
+	 *
+	 * @param   array  $config  An optional associative array of configuration settings.
+	 *
+	 * @since   1.6
+	 * @see     JController
+	 */
+	public function __construct($config = array())
+	{
+		if (empty($config['filter_fields']))
+		{
+			$config['filter_fields'] = array(
+				'id', 'i.id',
+				'title', 'i.title',
+				'alias', 'i.alias',
+				'catid', 'i.catid', 'category_title',
+				'state', 'i.state',
+				'created', 'i.created',
+				'ordering', 'i.ordering',
+				'published', 'i.published',
+				'category_id',
+				'level'
+			);
+		}
+
+		parent::__construct($config);
+	}
+	/**
 	 * Method to auto-populate the model state.
 	 *
 	 * Note. Calling getState in this method will result in recursion.
@@ -29,34 +57,22 @@ class LBGalleryModelItems extends JModelList
 		$app = JFactory::getApplication();
 
 		// Adjust the context to support modal layouts.
-		//if ($layout = $app->input->get('layout'))
-		//{
-		//	$this->context .= '.' . $layout;
-		//}
+		if ($layout = $app->input->get('layout'))
+		{
+			$this->context .= '.' . $layout;
+		}
 
+		// Add search filter
 		$search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
 		$this->setState('filter.search', $search);
 
-		//$access = $this->getUserStateFromRequest($this->context . '.filter.access', 'filter_access');
-		//$this->setState('filter.access', $access);
-
-		//$authorId = $app->getUserStateFromRequest($this->context . '.filter.author_id', 'filter_author_id');
-		//$this->setState('filter.author_id', $authorId);
-
+		// Add published state filter
 		$published = $this->getUserStateFromRequest($this->context . '.filter.published', 'filter_published', '');
 		$this->setState('filter.published', $published);
 
+		// Add category filter
 		$categoryId = $this->getUserStateFromRequest($this->context . '.filter.category_id', 'filter_category_id');
 		$this->setState('filter.category_id', $categoryId);
-
-		//$level = $this->getUserStateFromRequest($this->context . '.filter.level', 'filter_level');
-		//$this->setState('filter.level', $level);
-
-		//$language = $this->getUserStateFromRequest($this->context . '.filter.language', 'filter_language', '');
-		//$this->setState('filter.language', $language);
-
-		//$tag = $this->getUserStateFromRequest($this->context . '.filter.tag', 'filter_tag', '');
-		//$this->setState('filter.tag', $tag);
 
 		// List state information.
 		parent::populateState('i.id', 'desc');
